474d06564389332844c97f1103cca62d7414917b
Sergey Dzyuban <sergey.dziuban@intapp.com>
2022-02-19 11:29:54 +0200

    generate apps

---
+++ main.py
@@ -0,0 +1,16 @@
+def main(): 
+    f = open("./templates/app.template")
+    template = f.read()
+
+    for i in range(50):
+        name = "argo-performance-" + str(i)
+        result = template.replace("{{name}}", name)
+        target = open("./apps/" + name + ".yml", "w+")
+        target.write(result)
+        target.close()
+
+    f.close()
+    print("Done")
+
+if __name__ == "__main__":
+    main()
