627c63846d63d2cfadf4aaea2eca5488458811b2
Sergey Dzyuban <sergey.dziuban@intapp.com>
2022-02-20 21:12:30 +0200

    projects

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from random import randrange
+
 def main(): 
-    generate(200, "in-cluster")
-    generate(200, "gke_playground-s-11-8818bdd8_us-central1-c_cluster-1")
+    projects(10)
+    generate(2, "in-cluster")
+    generate(2, "gke_playground-s-11-8818bdd8_us-central1-c_cluster-1")
 
 def generate(n, cluster): 
     f = open("./templates/app.template")
@@ -8,9 +11,25 @@ def generate(n, cluster):
     clusterName = cluster[0:10].replace("_", "-")
 
     for i in range(n):
+        j = randrange(10)
         name = "argo-performance-" + clusterName + "-" + str(i)
-        result = template.replace("{{name}}", name).replace("{{cluster}}", cluster)
-        target = open("./apps/" + name + ".yml", "w+")
+        result = template.replace("{{name}}", name).replace("{{cluster}}", cluster).replace("{{project}}", "project-" + str(j))
+        target = open("./apps/generated/" + name + ".yml", "w+")
+        target.write(result)
+        target.close()
+
+    f.close()
+    print("Done")
+
+
+def projects(n):
+    f = open("./templates/project.template")
+    template = f.read()
+
+    for i in range(n):
+        name = "project-" + str(i)
+        result = template.replace("{{name}}", name)
+        target = open("./apps/argocd/" + name + ".yml", "w+")
         target.write(result)
         target.close()
 
