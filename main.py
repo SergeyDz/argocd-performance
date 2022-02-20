880b63cb2dfedf1d5379980dc1e4f4b47fc7bdde
Sergey Dzyuban <sergey.dziuban@intapp.com>
2022-02-20 21:29:50 +0200

    30 projects

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import randrange
 
 def main(): 
-    projects(10)
+    projects(30)
     generate(400, "in-cluster")
     generate(200, "gke_playground-s-11-8818bdd8_us-central1-c_cluster-1")
 
@@ -11,7 +11,7 @@ def generate(n, cluster):
     clusterName = cluster[0:10].replace("_", "-")
 
     for i in range(n):
-        j = randrange(10)
+        j = randrange(30)
         name = "argo-performance-" + clusterName + "-" + str(i)
         result = template.replace("{{name}}", name).replace("{{cluster}}", cluster).replace("{{project}}", "project-" + str(j))
         target = open("./apps/generated/" + name + ".yml", "w+")
