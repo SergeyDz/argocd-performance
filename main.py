880739d2a2fbaa2a8608decb8f48a9cd6903f478
Sergey Dzyuban <sergey.dziuban@intapp.com>
2022-02-21 00:06:53 +0200

    2k rows

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from random import randrange
 def main(): 
     projects(30)
     generate(100, "in-cluster")
-    generate(800, "gke_playground-s-11-8818bdd8_us-central1-c_cluster-1")
+    generate(2000, "gke_playground-s-11-8818bdd8_us-central1-c_cluster-1")
 
 def generate(n, cluster): 
     f = open("./templates/app.template")
