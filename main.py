bec6a258daf8b24f2d09aab18799071089747b70
Sergey Dzyuban <sergey.dziuban@intapp.com>
2022-02-20 14:52:10 +0200

    200 images

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 def main(): 
+    default(200, "in-cluster")
+
+def default(n, cluster): 
     f = open("./templates/app.template")
     template = f.read()
 
-    for i in range(200):
-        name = "argo-performance-" + str(i)
-        result = template.replace("{{name}}", name)
+    for i in range(n):
+        name = "argo-performance-" + cluster + "-" + str(i)
+        result = template.replace("{{name}}", name).replace("{{cluster}}", cluster)
         target = open("./apps/" + name + ".yml", "w+")
         target.write(result)
         target.close()
