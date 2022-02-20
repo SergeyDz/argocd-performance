add446828766f3c2f0a7e9fc742a81fa14ea3a1b
Sergey Dzyuban <sergey.dziuban@intapp.com>
2022-02-20 21:17:59 +0200

    in cluster scale up

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from random import randrange
 
 def main(): 
     projects(10)
-    generate(2, "in-cluster")
+    generate(200, "in-cluster")
     generate(2, "gke_playground-s-11-8818bdd8_us-central1-c_cluster-1")
 
 def generate(n, cluster): 
